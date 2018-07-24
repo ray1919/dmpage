9fca1a7132aa1fae55cb87f836ba6e1732cdce90
Zhao Rui <zzqr@163.com>
2018-07-24 15:43:57 +0800

    v0.1.7

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,13 +4,11 @@ import { NgModule } from '@angular/core';
 import { AppComponent } from './app.component';
 import { HttpClientModule } from '@angular/common/http';
 import { AppRoutingModule, routingComponents } from './app-routing.module';
-import { DruglistComponent } from './druglist/druglist.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    routingComponents,
-    DruglistComponent
+    routingComponents
   ],
   imports: [
     BrowserModule,
